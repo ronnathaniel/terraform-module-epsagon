1e84a7c620408289230784736414b6ad6a7d14a0
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-23 13:01:36 -0400

    created both python3 and nodejs12 lambda layer version -- BUG: not able to find local layer package build in submodule

--- aws/modules/tracing_agent_layer/layer.tf
+++ aws/modules/tracing_agent_layer/layer.tf
@@ -1,9 +1,20 @@
 
 resource "aws_lambda_layer_version" "epsagon_agent_python" {
+//  provider = aws.main
   layer_name = "epsagon_agent_layer_python"
+  filename = "layer_package_python.zip"
+  compatible_runtimes = ["python3.6", "python3.7", "python3.8"]
+  description = "epsagon layer for python"
+  source_code_hash = filebase64sha256("${path.module}//layer_package_python.zip")
+
 }
 
 
 resource "aws_lambda_layer_version" "epsagon_agent_node" {
+//  provider = aws.main
   layer_name = "epsagon_agent_layer_node"
+  filename = "layer_package_node.zip"
+  compatible_runtimes = ["nodejs12.x"]
+  description = "epsagon layer for node"
+  source_code_hash = filebase64sha256("${path.module}//layer_package_node.zip")
 }
