d5161c5d74419059eb441afe49485459352903d5
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-23 13:21:45 -0400

    redirected and fixed library and package install

--- aws/modules/tracing_agent_layer/run_build.sh
+++ aws/modules/tracing_agent_layer/run_build.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 get_os() {
-  # getting local OS running script
   unameOutput="$(uname -s)"
 
   case "${unameOutput}" in
@@ -12,7 +11,6 @@ get_os() {
   *) os="FOREIGN OS:${unameOutput}" ;;
   esac
 
-  # sets first func parameter to OS result
   eval "${1}=${os}"
 }
 
@@ -28,20 +26,13 @@ create_layer_deployments() {
     rm -rf layer_package_node.zip
   fi
 
-  cd src/python || cd .
-#  zip -r layer_package_python.zip ./*
-  zip "${start_position}/layer_package_python.zip" ./*
+  cd src/python/packages || cd .
+  zip "${start_position}/build/layer_package_python.zip" ./*
 
-#  cd ../node/node_modules || cd .
   cd "${start_position}/src/node/node_modules" || cd .
-
-#  zip -r layer_package_node.zip ./*
-  zip "${start_position}/layer_package_node.zip" ./*
+  zip "${start_position}/build/layer_package_node.zip" ./*
 
   cd "${start_position}" || cd .
-
-  #  zip -j layer_package_python.zip ./src/python/*
-  #  zip -j layer_package_node.zip ./src/node/node_modules/*
 }
 
 delete_build() {
@@ -51,14 +42,9 @@ delete_build() {
   # to delete all but 'dir' in directory
   # find ./PARENT/DIR -type d ! -name DIR -delete
 
-#  if [[ -d src/python ]]; then
-  rm -rf ./src/python/*
-#  fi
+  rm -rf ./src/python/packages/*
 
-#  if [[ -d src/node ]]; then
   find ./src/node -type f ! -name package.json -delete
-#    fi
-  #  find ./src/node/node -type d ! -name node_modules -delete
   rm -rf ./src/node/node_modules/*
 }
 
@@ -66,9 +52,9 @@ build_python() {
   get_os os
 
   if [[ $os == "Linux" || $os == "Cygwin" ]]; then
-    python -m pip install --target=./src/python/ -r src/requirements.txt
+    python -m pip install --target=./src/python/packages -r src/python/requirements.txt
   elif [[ $os == "Mac" ]]; then
-    python3 -m pip install --target=./src/python/ -r src/requirements.txt
+    python3 -m pip install --target=./src/python/packages -r src/python/requirements.txt
   fi
 }
 
