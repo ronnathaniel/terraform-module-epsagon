8e2531dc05bdd0b5aa714fd2366221bdec2f64e1
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-23 12:59:35 -0400

    migrated lambda assume-role-policy to seperate json file, added agent module layer to function, added epsagon auto tracing env var

--- aws/examples/lambda/lambda.tf
+++ aws/examples/lambda/lambda.tf
@@ -2,35 +2,22 @@
 resource "aws_iam_role" "lambda" {
   name = "lambda_iam"
 
-  assume_role_policy = <<ROLEPOLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "sts:AssumeRole"
-      ],
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Sid": ""
-    }
-  ]
-}
-ROLEPOLICY
+  assume_role_policy = file("${path.module}/lambda_assume_policy.json")
 }
 
 module "agent" {
-//  source = "github.com/ronnathaniel/terraform-module-epsagon/tree/main/modules/tracing_agent_layer"
-  source = "../../modules/tracing_agent_layer/"
+  source = "../../modules/tracing_agent_layer"
+
+  //  providers = {
+  //    aws = aws.main
+  //  }
 }
 
-data "archive_file" "lambda-archive" {
-  type = "zip"
-  source_file = "src/main.py"
-  output_path = "function_package.zip"
-}
+//data "archive_file" "lambda-archive" {
+//  type = "zip"
+//  source_file = "${path.module}/src/main.py"
+//  output_path = "${path.module}/function_package.zip"
+//}
 
 resource "aws_lambda_function" "tf_test" {
   function_name = "tf_test_ron"
@@ -39,10 +26,18 @@ resource "aws_lambda_function" "tf_test" {
   runtime       = "python3.8"
 
   filename         = "function_package.zip"
-  source_code_hash = filebase64sha256("function_package.zip")
+  source_code_hash = filebase64sha256("build/function_package.zip")
+
+  layers = [
+    module.agent.layer_python_arn
+  ]
+
   environment {
     variables = {
-      KEY = "val"
+      KEY                 = "val"
+      EPSAGON_TOKEN       = "87a6f6b4-b796-455d-9d5e-bba5119c7d87"
+      EPSAGON_APP_NAME    = "terraform-store"
+      AUTOWRAPT_BOOTSTRAP = "epsagon"
     }
   }
 }
