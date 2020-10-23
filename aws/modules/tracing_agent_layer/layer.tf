51ad9e1e14ec77bda667cc914b79403b136058d3
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-23 16:08:32 -0400

    fixed lambda layer to point to correct filename

--- aws/modules/tracing_agent_layer/layer.tf
+++ aws/modules/tracing_agent_layer/layer.tf
@@ -2,7 +2,7 @@
 resource "aws_lambda_layer_version" "epsagon_agent_python" {
 //  provider = aws.main
   layer_name = "epsagon_agent_layer_python"
-  filename = "layer_package_python.zip"
+  filename = "${path.module}/build/layer_package_python.zip"
   compatible_runtimes = ["python3.6", "python3.7", "python3.8"]
   description = "epsagon layer for python"
   source_code_hash = filebase64sha256("${path.module}/build/layer_package_python.zip")
@@ -13,7 +13,7 @@ resource "aws_lambda_layer_version" "epsagon_agent_python" {
 resource "aws_lambda_layer_version" "epsagon_agent_node" {
 //  provider = aws.main
   layer_name = "epsagon_agent_layer_node"
-  filename = "layer_package_node.zip"
+  filename = "${path.module}/build/layer_package_node.zip"
   compatible_runtimes = ["nodejs12.x"]
   description = "epsagon layer for node"
   source_code_hash = filebase64sha256("${path.module}/build/layer_package_node.zip")
