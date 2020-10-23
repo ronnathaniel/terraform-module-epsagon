956b7f3a871fa8fb34e7b5734486f0c473e7ac8e
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-23 12:56:00 -0400

    renamed build scripts

--- aws/examples/lambda/run_build.sh
+++ aws/examples/lambda/run_build.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
 
-prepare_deployment_package() {
-
+delete_old_package() {
   if [[ -e function.zip ]]; then
     rm function_package.zip
   fi
+}
+
+prepare_deployment_package() {
+
   zip -r -j function_package.zip src/*
 }
 
 
 build() {
 
-  terraform fmt
-  terraform validate
-  terraform apply
-
+  delete_old_package
+  prepare_deployment_package
 }
 
 
-prepare_deployment_package
 build
