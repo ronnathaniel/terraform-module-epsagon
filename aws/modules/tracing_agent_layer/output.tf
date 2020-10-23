5f61bb88878877d5597156c3cef6321dd896c0bd
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-23 13:03:40 -0400

    created output for tracing layers module

---
+++ aws/modules/tracing_agent_layer/output.tf
@@ -0,0 +1,8 @@
+
+output "layer_python_arn" {
+    value = aws_lambda_layer_version.epsagon_agent_python.arn
+}
+
+output "layer_node_arn" {
+    value = aws_lambda_layer_version.epsagon_agent_node.arn
+}
