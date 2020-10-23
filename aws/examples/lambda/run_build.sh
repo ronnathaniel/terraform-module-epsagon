39b98dcbc3bc2a7649fd5af65a7db4f7929dc8d1
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-23 16:06:01 -0400

    fixed deployment package creation

--- aws/examples/lambda/run_build.sh
+++ aws/examples/lambda/run_build.sh
@@ -2,19 +2,21 @@
 
 
 delete_old_package() {
-  if [[ -e function.zip ]]; then
-    rm function_package.zip
+  if [[ -e build/function_package.zip ]]; then
+    rm build/function_package.zip
   fi
 }
 
 prepare_deployment_package() {
+  start_position="$(pwd)"
 
-  zip -r -j function_package.zip src/*
+  cd src || cd .
+  zip -r "${start_position}/build/function_package.zip" ./*
+  cd $start_position || cd .
 }
 
 
 build() {
-
   delete_old_package
   prepare_deployment_package
 }
