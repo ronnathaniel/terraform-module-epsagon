6e291b641cd39e58e69818a3ab9f379a20d62d79
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-24 12:27:32 -0400

    tf formatted

--- modules/aws_trace_layer/outputs.tf
+++ modules/aws_trace_layer/outputs.tf
@@ -1,8 +1,8 @@
 
 output "layer_python_arn" {
-    value = aws_lambda_layer_version.epsagon_agent_python.arn
+  value = aws_lambda_layer_version.epsagon_agent_python.arn
 }
 
 output "layer_node_arn" {
-    value = aws_lambda_layer_version.epsagon_agent_node.arn
+  value = aws_lambda_layer_version.epsagon_agent_node.arn
 }
