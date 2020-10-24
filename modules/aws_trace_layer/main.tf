8734a35d6a859b752935895d21d27cff2b1c7a71
Ron Nathaniel <rnathaniel7@gmail.com>
2020-10-24 12:27:12 -0400

    tf formatted

--- modules/aws_trace_layer/main.tf
+++ modules/aws_trace_layer/main.tf
@@ -1,20 +1,20 @@
 
 resource "aws_lambda_layer_version" "epsagon_agent_python" {
-//  provider = aws.main
-  layer_name = "epsagon_agent_layer_python"
-  filename = "${path.module}/build/layer_package_python.zip"
+  //  provider = aws.main
+  layer_name          = "epsagon_agent_layer_python"
+  filename            = "${path.module}/build/layer_package_python.zip"
   compatible_runtimes = ["python3.6", "python3.7", "python3.8"]
-  description = "epsagon layer for python"
-  source_code_hash = filebase64sha256("${path.module}/build/layer_package_python.zip")
+  description         = "epsagon layer for python"
+  source_code_hash    = filebase64sha256("${path.module}/build/layer_package_python.zip")
 
 }
 
 
 resource "aws_lambda_layer_version" "epsagon_agent_node" {
-//  provider = aws.main
-  layer_name = "epsagon_agent_layer_node"
-  filename = "${path.module}/build/layer_package_node.zip"
+  //  provider = aws.main
+  layer_name          = "epsagon_agent_layer_node"
+  filename            = "${path.module}/build/layer_package_node.zip"
   compatible_runtimes = ["nodejs12.x"]
-  description = "epsagon layer for node"
-  source_code_hash = filebase64sha256("${path.module}/build/layer_package_node.zip")
+  description         = "epsagon layer for node"
+  source_code_hash    = filebase64sha256("${path.module}/build/layer_package_node.zip")
 }
